8ef4b1a27e4f1400e01b3d7f23ddb3deefd7e34a
hokeelson <heshaochen853@gmail.com>
2026-08-24 00:25:53 +0800

    Show portfolio risk and strategy health panels

--- dashboard_realtime.py
+++ dashboard_realtime.py
@@ -1,5 +1,7 @@
 from dashboard_v8 import *  # noqa: F401,F403
 
 from src.realtime_dashboard import render_realtime_panel
+from src.pro_risk_dashboard import render_professional_risk_panel
 
 render_realtime_panel()
+render_professional_risk_panel()
